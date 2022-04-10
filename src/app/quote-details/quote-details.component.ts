3bf996df1fb33d743a4413ca77e6842b248d1d27
MugureNjunge <maureennjunge22@gmail.com>
2022-04-11 00:14:57 +0300

    add input binding property and event emitters

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-details',
@@ -6,6 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+
+  //input property binding
+  @Input()quote!: Quote;
+
+  //event emitters
+  //delete event emitter
+  @Output() deleteQuoteEvent = new EventEmitter<Quote>();
+
+  //upvote event emitter
+  @Output() upVoteQuoteEvent = new EventEmitter<Quote>();
+
+  //downvote event emitter
+  @Output() downVoteQuoteEvent = new EventEmitter<Quote>();
+
+
+  deleteQuote(quote:Quote) {
+    this.deleteQuoteEvent.emit(quote);
+  }
+
+  upVoteQuote(quote:Quote) {
+    this.upVoteQuoteEvent.emit(quote);
+  }
+
+  downVoteQuote(quote:Quote) {
+    this.downVoteQuoteEvent.emit(quote);
+  }
 
   constructor() { }
 
