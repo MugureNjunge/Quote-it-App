1256dc3e66385f38e7fdb68678f7cd76ae48557a
MugureNjunge <maureennjunge22@gmail.com>
2022-04-11 00:23:17 +0300

    add functionality to quote details and quote form components

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteFormComponent implements OnInit {
 
+  newQuote = new Quote(0, "", "", "", new Date());
+
+  //event emitter
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  //displaySuccessMessage
+  displaySuccessMessage = false;
+  // constructor() { }
+
+  onSubmit() {
+    this.displaySuccessMessage = true;
+
+    //create a new quote
+    this.addQuote.emit(this.newQuote);
+  }
+
   constructor() { }
 
   ngOnInit(): void {
