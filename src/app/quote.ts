52a91a268cc9b2e454ea4fdeed1161471faead93
MugureNjunge <maureennjunge22@gmail.com>
2022-04-11 01:07:18 +0300

    use pipes to display date data

---
+++ src/app/quote.ts
@@ -0,0 +1,14 @@
+export class Quote {
+  showVotes: boolean;
+  constructor(
+    public id: number,
+    public author: string,
+    public createdBy: string,
+    public statement: string,
+    public createdAt: Date,
+    public likes: number = 0,
+    public dislikes: number = 0
+  ) {
+    this.showVotes = false;
+  }
+}
