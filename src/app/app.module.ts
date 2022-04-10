443408f2576cf02a3ef07f5ec6366722ef900d1b
MugureNjunge <maureennjunge22@gmail.com>
2022-04-11 00:37:01 +0300

    generate pipes and directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { QuotesComponent } from './quote/quote.component';
-import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { HighlightQuoteDirective } from './highlight-quote.directive';
+import { DayCountPipe } from './day-count.pipe';
+import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    QuoteComponent,
+    QuoteDetailComponent,
+    QuoteFormComponent,
+    HighlightQuoteDirective,
+    DayCountPipe,
     AboutComponent,
-    QuotesComponent,
-    QuoteDetailsComponent,
-    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
